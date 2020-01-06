a9484ccb49722cb253e4b59916500d041592b6e1
Farah-Oubelkas <farah.oubelkas@gmail.com>
2020-01-06 22:00:56 +0100

    Image Classofication using Webcam

--- farah-example/sketch.js
+++ farah-example/sketch.js
@@ -1,38 +1,42 @@
 let mobilenet;
 
 
-let penguin;
+let video;
+let label = ' ';
 
 function modelReady() {
     console.log('Model is ready !!!');
-    mobilenet.predict(penguin, gotResult);
+    mobilenet.predict(gotResult);
 }
 
 function gotResult(error, results) {
     if (error) {
         console.error(error);
     } else {
-        console.log(results);
-        let label = results[0].label;
-        let proba = results[0].confidence;
-        fill(0);
-        textSize(64);
-        text(label,10,height - 320);
-        createP(label);
-        createP(proba);
+//        console.log(results);
+        label = results[0].label;
+        mobilenet.predict(gotResult);
+
     }
 }
 
-function imageReady() {
-    image(penguin, 0, 0, width, height);
-    console.log('image is ready!!!');
-}
+//function imageReady() {
+//    image(penguin, 0, 0, width, height);
+//    console.log('image is ready!!!');
+//}
 
 function setup() {
-    createCanvas(400, 400);
-    penguin = createImg('images/penguin.jpg', imageReady);
-    penguin.hide();
+    createCanvas(640, 550);
+    video = createCapture(VIDEO);
+    video.hide();
     background(0);
-    mobilenet = ml5.imageClassifier('MobileNet', modelReady);
+    mobilenet = ml5.imageClassifier('MobileNet', video, modelReady);
+}
 
+function draw() {
+    background(0);
+    image(video, 0, 0);
+    fill(255);
+    textSize(64);
+    text(label, 10, height - 20);
 }
